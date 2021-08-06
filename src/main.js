bca2a4b45ffede637eb5e3e1cc3cc186880f5855
Hendrik Braasch <fabriogo@outlook.com>
2021-08-06 20:43:23 +1200

    Refactor main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store.js';
+import EventList from './components/EventList.vue'
+import EventForm from './components/EventForm.vue'
+import Navigation from './components/Navigation.vue'
 
-createApp(App).mount('#app')
+const countDownerApp = createApp(App)
+countDownerApp.component('EventList', EventList)
+countDownerApp.component('EventForm', EventForm)
+countDownerApp.component('Navigation', Navigation)
+countDownerApp.use(store)
+countDownerApp.mount('#app')
