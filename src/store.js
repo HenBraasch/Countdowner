b5f9fd03bd33e258d216b2060704c23dd6448279
Hendrik Braasch <fabriogo@outlook.com>
2021-08-22 09:22:10 +1200

    Fix issue with event dates

--- src/store.js
+++ src/store.js
@@ -13,7 +13,12 @@ export default createStore({
     getters:{},
     mutations: {
         setInitialEvents(state, events){
-            state.events = events.sort((a,b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0));
+            const eventMap = events.map(function(x) {
+               x.date = Date.parse(x.date)
+               return x
+            })
+            
+            state.events = eventMap
         },
         removeEvent(state, item){
             state.events = state.events.filter(event => event !== item)
