f69a467001362489aa66a453c9d16f1b624f9850
Hendrik Braasch <fabriogo@outlook.com>
2021-08-24 20:57:27 +1200

    Add styling to the hero img selection

--- src/components/EventForm.vue
+++ src/components/EventForm.vue
@@ -5,13 +5,10 @@
             <div class="form-group">
                 <input class="form-control" v-model="event.title" type="text"  placeholder="Add Title" maxlength="25"> ({{event.title.length}}/25 Words)
                 <input class="form-control" type="date" v-model="event.date">
-                <div class="row">
-                    <div v-for="(hero) in $store.state.heros" :key="hero.id">
-                        <div class="col-xs-2">
-                            <div class="card hero-card h-100">
-                                <img class="card-img-top hero" :src=hero.url alt={{hero.title}} :class="{selected: hero.id == selectedHero.id}" @click="event.picture_url = hero.url, selectedHero = hero">
-                            </div>
-                        </div>
+                
+                <div class="container">
+                    <div class="hero-gallery">
+                        <img  v-for="(hero) in $store.state.heros" :key="hero.id" :src=hero.url alt={{hero.title}} :class="{selected: hero.id == selectedHero.id}" @click="event.picture_url = hero.url, selectedHero = hero">
                     </div>
                 </div>
                 <input class="form-control btn btn-primary" type="submit" @click.prevent="$emit('eventAdded'),$store.commit('addEvent', event), saveNewEvent()">
@@ -53,8 +50,29 @@ export default{
  img.hero{
      max-width: 150px;
  }
- .hero-card{
-     max-width: 10%;
- }
+
+
+div.hero-gallery img {
+  display: inline-block;
+  color: white;
+  text-align: center;
+  padding: 14px;
+  text-decoration: none;
+  max-height: 250px;
+  max-width: 250px;
+}
+
+div.hero-gallery {
+  background-color:white;
+  overflow-x: scroll;
+  white-space: nowrap;
+  height: 250px
+}
+
+div.hero-gallery::-webkit-scrollbar{
+    display: none;
+}
+ 
+
 
 </style>
